aebc243e23aae5863c9d93d9090ac24078373fd6
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-05-31 23:51:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,23 +5,40 @@ import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
 
 const inputSearchBox = document.querySelector('input#search-box');
+const cardList = document.querySelector('.country-list');
+console.log(cardList);
 
 inputSearchBox.addEventListener('input', debounce(onSearchBox, DEBOUNCE_DELAY));
 
 function onSearchBox(evt) {
-    const name = evt.target.value;
+    const inputValue = evt.target.value;
    
-    fetchCountries(name)
-        .then(countries => {
-            renderMarkup(countries);
+    fetchCountries(inputValue)
+        .then((item) => {
+            renderMarkup(item);
+            console.log(item);
         })
 }
 
 
 function renderMarkup(data) {
-    return data.map(item => `
-    <div class="country-info"> ${item.name}</div>
+    const markup = data
+        .map(
+        ({ name, capital, population, languages, flags }) => `
+    <div class="country-info">
+    <img class="country-img"
+     src="${flags.svg}"
+     alt="flags"
+     width = 40px
+     height = 40px >
+    <div class="country-name"> ${name.official}</div>
+    <div class="country-capital">Capital: ${capital}</div>
+    <div class="country-population">Population: ${population}</div>
+     <div class="country-languages">Languages: ${languages}</div>
+     </div>
     `).join('');
+
+    cardList.insertAdjacentHTML('beforeend', markup);
 }
 
 
