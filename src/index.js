ac86b35791a32c4e481ea5ca2d3154d4bf12ef58
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-05-31 11:17:31 +0300

    added changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
+import { fetchCountries }  from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputSearchBox = document.querySelector('input#search-box');
+
+inputSearchBox.addEventListener('input', onSearchBox);
+
+function onSearchBox(evt) {
+    const name = evt.currentTarget.value;
+    fetchCountries(name);
+}
+
+
+
+
+
