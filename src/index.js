af94f194b00cc263b20752096dc7d6e3c0acf293
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-05-31 20:01:07 +0300

    added changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import './css/styles.css';
-import { fetchCountries }  from './fetchCountries';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
 const inputSearchBox = document.querySelector('input#search-box');
 
-inputSearchBox.addEventListener('input', onSearchBox);
+inputSearchBox.addEventListener('input', debounce(onSearchBox, DEBOUNCE_DELAY));
 
 function onSearchBox(evt) {
-    const name = evt.currentTarget.value;
-    fetchCountries(name);
+    const name = evt.target.value;
+   
+    fetchCountries(name)
+        .then(countries => {
+            renderMarkup(countries);
+        })
 }
 
 
-
+function renderMarkup(data) {
+    return data.map(item => `
+    <div class="country-info"> ${item.name}</div>
+    `).join('');
+}
 
 
