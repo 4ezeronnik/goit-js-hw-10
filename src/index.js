97ad7b549f19b7cab01b8c1464a7eef14dd7fd2d
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-06-01 16:11:01 +0300

    input new changes

--- src/index.js
+++ src/index.js
@@ -15,24 +15,32 @@ function onSearchBox(evt) {
     const inputValue = evt.target.value.trim();
 
      if (!inputValue) {
-         document.location.reload()
+         document.reload();
      }
 
     fetchCountries(inputValue)
         .then((item) => {
-            if (item.length === 1)
+            if (item.length === 1) {
                 renderMarkupForOne(item)
+                
+            };
             if (item.length >=2 && item.length <=10) {
-            renderMarkupForAll(item)
+                renderMarkupForAll(item)
+                
         }
             if (item.length > 10) {
-                 Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
-           }
+                Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+               cardList.innerHTML = "";
+            }
+        
             console.log(item);
-        })
+        }
+        )
         .catch(error => {
-            Notiflix.Notify.failure("Oops, there is no country with that name")
-                console.log(error);;
+            Notiflix.Notify.failure("Oops, there is no country with that name");
+            console.log(error);
+            cardList.innerHTML = "";
+            
         })
     
 }
